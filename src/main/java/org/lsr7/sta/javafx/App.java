d15cac39b9113101149ae0c3df02d3c3981c602c
ksylvester372 <ksylvester372@lsr7sd.org>
2024-04-23 09:05:45 -0500

    Update App.java

--- src/main/java/org/lsr7/sta/javafx/App.java
+++ src/main/java/org/lsr7/sta/javafx/App.java
@@ -1,10 +1,13 @@
 package org.lsr7.sta.javafx;
-
+import javafx.scene.text.Text;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
+import javafx.scene.control.Button;
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -20,10 +23,25 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         System.out.println("start");
-        Parent root = new Group();
-        Scene scene = new Scene(root, 640, 480);
+        Text text = new Text(10,40,"Hello papa");
+        text.setFont(new Font(45));
+        
+        Button close = new Button("press me papa ;)");
+        
+        Parent root = new Group(text,close);
+     
+        Scene scene = new Scene(root, 800, 600);
+
+      //  Image image = new Image("C:\Users\ksylvester372\Downloads\download.png");
         stage.setScene(scene);
+        stage.setTitle("Pappa Kendle");
         stage.show();
+        
     }
+    @Override
+    public void stop()  {
+        System.out.println("stop");
+    }
+
 
 }
